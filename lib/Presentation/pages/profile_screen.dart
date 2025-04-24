a084450a206309d72a62004d49cd04316b52175a
AymanAltairi73 <aymanaltairi7368@gmail.com>
2025-04-25 01:09:05 +0300

    feat: Add font_awesome_flutter package
    - Added the font_awesome_flutter package to the project. This package provides a collection of Font Awesome icons that can be used in the application's UI.

--- lib/Presentation/pages/profile_screen.dart
+++ lib/Presentation/pages/profile_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../core/utility/navigation_helper.dart';
-
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -9,35 +9,24 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return WillPopScope(
+    return WillPopScope(
       onWillPop: () async {
         NavigationHelper.navigateToPage(context, 0);
         return false;
       },
       child: Scaffold(
-      backgroundColor: const Color(0xFF543A20),
-      appBar: _buildAppBar(context),
-      body: _buildBody(context),
-//      bottomNavigationBar: CustomBottomNavBar(
-//   selectedIndex: _selectedIndex,
-//   onItemSelected: (index) {
-//     NavigationHelper.navigateToPage(
-//       context,
-//       index,
-//       coffeeList: [],  // Since ProfileScreen doesn't manage coffee list
-//       onCartChanged: (_, {isSelected, quantity}) {},  // Empty callback
-//       onFavoriteChanged: (_) {},  // Empty callback
-//     );
-//   },
-// ),
-     ));
+        backgroundColor: const Color(0xFF543A20),
+        appBar: _buildAppBar(context),
+        body: _buildBody(context),
+      ),
+    );
   }
 
   AppBar _buildAppBar(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
-     leading: IconButton(
+      leading: IconButton(
         icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
         onPressed: () => NavigationHelper.goBack(context),
       ),
@@ -45,15 +34,15 @@ class ProfileScreen extends StatelessWidget {
         'Profile',
         style: TextStyle(
           color: Colors.white,
-          fontSize: 20,
+          fontSize: 22,
           fontWeight: FontWeight.bold,
         ),
       ),
       centerTitle: true,
-     actions: [
+      actions: [
         IconButton(
-          icon: const Icon(Icons.edit, color: Colors.white),
-          onPressed: () => _navigateToEditProfile(context),
+          icon: const Icon(Icons.notifications_none, color: Colors.white, size: 28,),
+          onPressed: () => Navigator.pushNamed(context, '/notifications'),
         ),
       ],
     );
@@ -74,7 +63,7 @@ class ProfileScreen extends StatelessWidget {
 
   Widget _buildProfileHeader() {
     return Container(
-            padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(20),
 
       child: Column(
         children: [
@@ -84,18 +73,22 @@ class ProfileScreen extends StatelessWidget {
                 radius: 50,
                 backgroundImage: AssetImage('assets/images/profile.png'),
               ),
-              // Positioned(
-              //   right: 0,
-              //   bottom: 0,
-              //   child: Container(
-              //     padding: const EdgeInsets.all(4),
-              //     decoration: const BoxDecoration(
-              //       color: Color(0xFFCE9760),
-              //       shape: BoxShape.circle,
-              //     ),
-              //     child: const Icon(Icons.edit, color: Colors.white, size: 20),
-              //   ),
-              // ),
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(4),
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFCE9760),
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+              ),
             ],
           ),
           const SizedBox(height: 15),
@@ -107,83 +100,52 @@ class ProfileScreen extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-           const SizedBox(height: 5),
-          Text(
-            'aymanaltairi8@gmail.com',
-            style: TextStyle(
-              color: Colors.white.withOpacity(0.7),
-              fontSize: 16,
-            ),
-          ),
+          const SizedBox(height: 5),
         ],
       ),
     );
   }
-  void _navigateToEditProfile(BuildContext context) {
-    Navigator.pushNamed(context, '/edit-profile');
-  }
 
-  void _navigateToPaymentMethods(BuildContext context) {
-    Navigator.pushNamed(context, '/payment-methods');
-  }
+  // void _navigateToEditProfile(BuildContext context) {
+  //   Navigator.pushNamed(context, '/edit-profile');
+  // }
 
-  void _navigateToAddresses(BuildContext context) {
-    Navigator.pushNamed(context, '/addresses');
-  }
-
-  void _navigateToOrderHistory(BuildContext context) {
-    Navigator.pushNamed(context, '/order-history');
-  }
-
-  void _navigateToSettings(BuildContext context) {
-    Navigator.pushNamed(context, '/settings');
-  }
-
-  void _navigateToSupport(BuildContext context) {
-    Navigator.pushNamed(context, '/support');
-  }
-
- 
   Widget _buildMenuItems(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: [
           _buildMenuItem(
-            icon: Icons.payment,
-            title: 'Payment Methods',
-            onTap: () => _navigateToPaymentMethods(context),
+            icon: Icons.person_outline,
+            title: 'Your Profile',
+            onTap: () {},
           ),
           _buildMenuItem(
-            icon: Icons.location_on,
-            title: 'Delivery Address',
-            onTap: () => _navigateToAddresses(context),
+            icon: Icons.shopping_bag_outlined,
+            title: 'My Orders',
+            onTap: () => Navigator.pushNamed(context, '/my-orders'),
+          ),
+
+          _buildMenuItem(
+            icon: Icons.settings_outlined,
+            title: 'Setting',
+            onTap: () => Navigator.pushNamed(context, '/settings'),
           ),
           _buildMenuItem(
-            icon: Icons.history,
-            title: 'Order History',
-            onTap: () => _navigateToOrderHistory(context),
+            icon: Icons.privacy_tip_outlined,
+            title: 'Privacy policy',
+            onTap: () => Navigator.pushNamed(context, '/privacy-policy'),
           ),
           _buildMenuItem(
-            icon: Icons.notifications,
-            title: 'Notifications',
-            onTap: () => NavigationHelper.goToNotifications(context),
-          ),
-          _buildMenuItem(
-            icon: Icons.settings,
-            title: 'Settings',
-            onTap: () => _navigateToSettings(context),
-          ),
-          _buildMenuItem(
-            icon: Icons.help,
-            title: 'Help & Support',
-            onTap: () => _navigateToSupport(context),
+            icon: Icons.person_add_outlined,
+            title: 'Invite Friend',
+            onTap: () => _showInviteFriendDialog(context),
           ),
           _buildMenuItem(
             icon: Icons.logout,
             title: 'Logout',
             onTap: () => _showLogoutDialog(context),
-            textColor: Colors.red,
+            //textColor: Colors.red,
           ),
         ],
       ),
@@ -194,93 +156,178 @@ class ProfileScreen extends StatelessWidget {
     required IconData icon,
     required String title,
     required VoidCallback onTap,
-     Color? textColor,
+    Color? textColor,
   }) {
     return ListTile(
-        leading: Icon(icon, color: textColor ?? Colors.white),
+      leading: Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          color: const Color(0xFFCE9760),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Icon(icon, color: Colors.white),
+      ),
       title: Text(
         title,
         style: TextStyle(
           color: textColor ?? Colors.white,
           fontSize: 16,
+          fontWeight: FontWeight.w500,
         ),
       ),
-      // leading: Container(
-      //   width: 40,
-      //   height: 40,
-      //   decoration: BoxDecoration(
-      //     color: const Color(0xFFCE9760),
-      //     borderRadius: BorderRadius.circular(20),
-      //   ),
-      //   child: Icon(icon, color: Colors.white),
-      // ),
-      //  title: Text(
-      //   title,
-      //   style: TextStyle(
-      //     color: textColor ?? Colors.white,
-      //     fontSize: 16,
-      //   ),
-      // ),
-        trailing: const Icon(
+
+      trailing: const Icon(
         Icons.arrow_forward_ios,
-        color: Colors.white,
+        color: Color(0xFFCE9760),
         size: 16,
       ),
       onTap: onTap,
     );
   }
-   void _showLogoutDialog(BuildContext context) {
+
+  void _showLogoutDialog(BuildContext context) {
     showDialog(
       context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Logout'),
-        content: const Text('Are you sure you want to logout?'),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text('Cancel'),
+      builder:
+          (context) => AlertDialog(
+            backgroundColor: const Color(0xFFCE9760),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            title: Center(
+              child: const Text(
+                'Logout',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            content: const Text(
+              'Are you sure you want to logout?',
+              style: TextStyle(color: Color(0xFF543A20), fontSize: 15),
+            ),
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFF543A20), width: 2),
+                      borderRadius: BorderRadius.circular(8),
+                      color: const Color(0xFF543A20),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      Navigator.pushNamed(context, '/login');
+                      },
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 30,
+                          vertical: 10,
+                        ),
+                      ),
+                      child: const Text(
+                        'Yes',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: const Color(0xFF543A20),
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 30,
+                          vertical: 10,
+                        ),
+                      ),
+                      child: const Text(
+                        'No',
+                        style: TextStyle(
+                          color: Color(0xFF543A20),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-          TextButton(
-            onPressed: () {
-             
-              Navigator.pop(context);
-              NavigationHelper.navigateToPage(context, 0);
-            },
-            child: const Text('Logout'),
-          ),
-        ],
-      ),
     );
   }
 
-//   Widget _buildBottomNavBar() {
-//     return Container(
-//       height: 70,
-//       decoration: BoxDecoration(
-//         color: const Color(0xFFCE9760),
-//         borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
-//         boxShadow: [
-//           BoxShadow(color: Colors.black.withOpacity(0.1), blurRadius: 10),
-//         ],
-//       ),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//         children: [
-//           _buildNavItem(Icons.home_outlined, true),
-//           _buildNavItem(Icons.shopping_cart_outlined, false),
-//           _buildNavItem(Icons.favorite_border, false),
-//           _buildNavItem(Icons.person_outline, false),
-//         ],
-//       ),
-//     );
-//   }
+  void _showInviteFriendDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            backgroundColor: const Color(0xFFCE9760),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            title: const Text(
+              'Share App',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Share this app with your friends',
+                  style: TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildShareButton(
+                       icon: FontAwesomeIcons.whatsapp,  // استخدم هذه الأيقو
+                      onTap: () => Navigator.pop(context),
+                    ),
+                    _buildShareButton(
+                      icon: Icons.facebook,
+                      onTap: () => Navigator.pop(context),
+                    ),
+                    _buildShareButton(
+                      icon: Icons.copy,
+                      onTap: () => Navigator.pop(context),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+    );
+  }
 
-//   Widget _buildNavItem(IconData icon, bool isSelected) {
-//     return Icon(
-//       icon,
-//       color: isSelected ? Colors.white : Colors.white.withOpacity(0.5),
-//       size: 28,
-//     );
-//   }
-// }
+  Widget _buildShareButton({
+    required IconData icon,
+    required VoidCallback onTap,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFF543A20),
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Colors.white, width: 1),
+      ),
+      child: IconButton(
+        icon: Icon(icon, color: Colors.white),
+        onPressed: onTap,
+      ),
+    );
+  }
 }
